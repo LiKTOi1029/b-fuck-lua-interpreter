e390e247d25b9a9b551dea86c5ce1a7295c6cd15
LiKTOi1029 <testingboy5689@gmail.com>
2025-07-23 12:30:22 +0330

    brainfuck.lua adding BFAdder() function

--- brainfuck.lua
+++ brainfuck.lua
@@ -1,4 +1,23 @@
 __BFFILE = arg[#arg]
+__BFCOMMANDS = {
+	["+"] = BFAdder,
+	["-"] = -1,
+	["<"] = 1,
+	[">"] = -1,
+	["."] = io.write,
+	[","] = io.read,
+	["["] = nil,
+	["]"] = nil,
+	
+}
+
+function BFAdder(Cell)
+	Cell = Cell + 1
+	if Cell > 255 then
+		Cell = 0
+	end
+
+
 
 if #arg ~= 1 then
 	error("Expected 1 file input, instead got "..#arg)
